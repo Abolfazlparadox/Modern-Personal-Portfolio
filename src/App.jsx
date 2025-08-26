843f85100e68c9ea621770bbeaee9d5d91d811b5
abolfazlparadox <fito.kamio7@gmail.com>
2025-08-26 22:11:27 +0330

    feat: initialize portfolio project with Vite, React, and Tailwind CSS
    - Added package.json with dependencies for React, React Router, Tailwind CSS, and other utilities.
    - Created initial project structure with App component, Home and NotFound pages.
    - Implemented routing using React Router.
    - Added SVG assets for Vite and React logos.
    - Configured Tailwind CSS with custom styles and animations in index.css.
    - Set up Vite configuration for React and Tailwind CSS integration.
    - Included installation instructions in paradox.txt for setting up the project.

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Home } from "./assets/pages/Home"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { NotFound } from "./assets/pages/NotFound"
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />}/>
+        <Route path="*" element={<NotFound />}/>
+      </Routes>
+    </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
